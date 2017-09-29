1ba40d59e616a3404c80e286198b71019d0ef769
Ken Miller <ken.miller521@gmail.com>
2017-09-29 00:45:27 -0700

    Imported Project Files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody2D rb2d;
+    public float movementSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(movementSpeed*-1, 0, 0));
+            //rb2d.AddForce(new Vector2(-Time.deltaTime*movementSpeed*100, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(movementSpeed, 0, 0));
+            //rb2d.AddForce(new Vector2(Time.deltaTime * movementSpeed*100, 0));
+        }
+        else
+            rb2d.velocity = Vector3.zero;
+
+    }
+}
