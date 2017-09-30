0da35f7d1be0382235860b39032182b9a84bf131
Ken Miller <ken.miller521@gmail.com>
2017-09-29 23:45:45 -0700

    Made final animation test

---
+++ Assets/Scripts/FinishedAnimationControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishedAnimationControllerScript : MonoBehaviour {
+    private Animator anim;
+    public AudioSource audioSource;
+    public AudioClip footstepsAudio;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("FacingRight", false);
+            anim.SetBool("isMoving", true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("FacingRight", true);
+            anim.SetBool("isMoving", true);
+        }
+        else
+            anim.SetBool("isMoving", false);
+    }
+
+    public void playFootstepsSound()
+    {
+        audioSource.PlayOneShot(footstepsAudio);
+    }
+}
